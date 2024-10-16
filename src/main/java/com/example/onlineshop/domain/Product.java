fe62102bae0178ca32f6195bf233b4317024cca7
None <134219525+None-44@users.noreply.github.com>
2024-10-16 23:30:13 +0800

    编译通过

--- src/main/java/com/example/onlineshop/domain/Product.java
+++ src/main/java/com/example/onlineshop/domain/Product.java
@@ -1,14 +1,11 @@
 package com.example.onlineshop.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Table(name = "Products")
 public class Product {
 
     @Id
